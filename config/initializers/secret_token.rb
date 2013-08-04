f4118428fd7a6f3fd796e6f3682edf20a1ce94d8
Cathy Cullen <cullen11>
2013-08-04 17:47:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CareerCheetah::Application.config.secret_key_base = '565ed91699071e98b4c21c0e659fe33b07ca87e929afbe7e957fa5018b7c0f92920ce7231f3d6f2125e71f5b5a592b0d1aa23c622b2d6c792332ddd82a2e05cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CareerCheetah::Application.config.session_store :cookie_store, key: '_CareerCheetah_session'
