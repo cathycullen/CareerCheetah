750be85f86ab6505d40186cf50dcc87235e62ba8
Nate Delage <nate@natedelage.com>
2013-08-06 11:45:41 -0500

    Career model tests

--- app/models/career.rb
+++ app/models/career.rb
@@ -3,5 +3,6 @@ class Career < ActiveRecord::Base
   has_many :factors, :through => :career_factor_mappings
 
   validates_presence_of :onet_code, :title, :description, :job_zone
-  validates_uniqueness_of :onet_code	
+  validates_uniqueness_of :onet_code, :title
+  validates_inclusion_of :job_zone, :in => [1,2,3,4,5]
 end
