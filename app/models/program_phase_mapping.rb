ddbef2200a9b49003c4b29be5d3fb06bbab84643
Nate Delage <nate@natedelage.com>
2013-08-07 11:43:09 -0500

    Initial stab at working back/forward links for questions within section

--- app/models/program_phase_mapping.rb
+++ app/models/program_phase_mapping.rb
@@ -7,4 +7,14 @@ class ProgramPhaseMapping < ActiveRecord::Base
   validates_presence_of :phase_id, :program_id
 
   ranks :row_order, :with_same => :program_id
+
+  def next
+    program.program_phase__mappings
+      .rank(:row_order).where(["row_order > ?", row_order]).first
+  end
+
+  def previous
+    program.program_phase__mappings
+      .rank(:row_order).where(["row_order < ?", row_order]).first
+  end
 end
