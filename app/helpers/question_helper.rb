1a24401c9827beee72334ae64411ba4fbf826271
Nate Delage <nate@natedelage.com>
2013-08-12 11:15:09 -0500

    Group options in columns when space allows

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -16,4 +16,27 @@ module QuestionHelper
   def response_cache
     @response_cache ||= UserResponseCache.new(current_user, @question)
   end
+
+  def option_groups(question)
+    max_length = question.response_options.map do |o|
+      o.description.length
+    end.max
+
+    group_size = 4
+    if max_length <= 15
+      group_size = option_row_count*4
+    elsif max_length <= 25
+      group_size = option_row_count*3
+    elsif max_length <= 38
+      group_size = option_row_count*2
+    end
+
+    question.response_options.rank(:row_order)
+      .each_slice(group_size).to_a
+  end
+
+  def option_row_count
+    4
+  end
+
 end
