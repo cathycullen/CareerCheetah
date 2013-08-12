f4bef5b44ef384d1929c1d4c77cf6c99f3c77546
Nate Delage <nate@natedelage.com>
2013-08-12 15:15:52 -0500

    SectionConclusion pages

--- app/controllers/program_navigations_controller.rb
+++ app/controllers/program_navigations_controller.rb
@@ -1,16 +1,15 @@
 class ProgramNavigationsController < ApplicationController
 
-  def next
-    load_context_object
-    n = Navigation::ProgramNavigator.new(@context_object)
+  before_filter :load_context_object
+  # before_filter :verify_completion_code, :only => :next
 
+  def next
+    n = Navigation::ProgramNavigator.new(@context_object)
     redirect_to n.next
   end
 
   def previous
-    load_context_object
     n = Navigation::ProgramNavigator.new(@context_object)
-
     redirect_to n.previous
   end
 
@@ -20,8 +19,18 @@ class ProgramNavigationsController < ApplicationController
       @context_object = Question.find(params[:question_id])
     elsif params[:section_id]
       @context_object = Section.find_by(:slug => params[:section_id])
+    elsif params[:section_conclusion_id]
+      @context_object = SectionConclusion.new(Section.find_by(:slug => params[:section_conclusion_id]))
     elsif params[:phase_id]
       @context_object = Phase.find_by(:slug => params[:phase_id])
+    end
+  end
+
+  def verify_completion_code
+    if @context_object.instance_of?(SectionConclusion)
+      if @context_object.section.completion_code != params[:completion_code]
+        redirect_to :back
+      end
     end
   end
 
---
+++ app/models/section_conclusion.rb
@@ -0,0 +1,7 @@
+class SectionConclusion
+  attr_reader :section
+
+  def initialize(section)
+    @section = section
+  end
+end
