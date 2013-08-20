d0f8cc05a392d9795f6f35280fef7caf880ea246
Cathy Cullen <cathy@softwareoptions.com>
2013-08-20 10:10:30 -0500

    added headline text to mood results

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130814192658) do
+ActiveRecord::Schema.define(version: 20130815134911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20130814192658) do
     t.string   "fit_code"
     t.string   "rating_prompt"
     t.string   "response_type"
+    t.string   "work_zone"
   end
 
   add_index "response_options", ["factor_id"], name: "index_response_options_on_factor_id", using: :btree
