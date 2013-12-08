138cfa76c13aa8a98db3ee64285c9dcf9679a5f2
Nate Delage <nate@natedelage.com>
2013-12-08 09:39:13 -0600

    Delete UserCareers when name is blank

--- app/controllers/user_careers_controller.rb
+++ app/controllers/user_careers_controller.rb
@@ -1,9 +1,16 @@
 class UserCareersController < ApplicationController
   def update
     @career = current_user.user_careers.find(params[:id])
-
     @career.name = params[:name]
-    if @career.save!
+
+    status = nil
+    if params[:name].empty?
+      status = @career.destroy
+    else
+      status = @career.save
+    end
+
+    if status
       render json: @career
     else
       render json: {message: "Unable to save user career"}, status: 400
